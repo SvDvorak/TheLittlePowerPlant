7aeb8f615d16f54a1b6139d6f6ff7a5616a64dee
Andreas Wilcox <unknownbum@gmail.com>
2015-02-25 15:13:04 +0100

    Made overload check based on time in overload

--- Assets/Code/ScoreUpdater.cs
+++ Assets/Code/ScoreUpdater.cs
@@ -10,19 +10,23 @@ public static class ScoreManager
 
 public class ScoreUpdater : MonoBehaviour
 {
+	public float MaxOutput = 400f;
 	public float EnergySellPrice = 1f;
 	public float EnergyBuyCost = 2f;
-	public float OutputOverload;
-	public float MaxOutput = 400f;
+	public float OutputOverloadLimit;
+	public float OverloadMaxTime = 3f;
+	public float OverloadAmount;
 
-    public float CityValue { get { return ScoreManager.CityValue; } }
+	private float OverloadPerSecond { get { return 1/OverloadMaxTime;} }
+
+	public float CityValue { get { return ScoreManager.CityValue; } }
     public float Output { get; private set; }
     public float Income { get; set; }
 
 	public float MinimumOutputRequired { get { return CityValue / 100f; } }
 	public Range CurrentOutputInUnit { get { return new Range(0, Output/MaxOutput); } }
 	public Range MinimumOutputInUnit { get { return new Range(0, MinimumOutputRequired / MaxOutput); } }
-	public Range OverloadOutputInUnit { get { return new Range(OutputOverload / MaxOutput, 1.0f); } }
+	public Range OverloadOutputInUnit { get { return new Range(OutputOverloadLimit / MaxOutput, 1.0f); } }
 
 	private readonly Dictionary<object, float> _machineOutputs = new Dictionary<object, float>();
 
@@ -31,8 +35,8 @@ public class ScoreUpdater : MonoBehaviour
         ScoreManager.CityValue = 5000;
         Output = 0;
         Income = 250000;
-	    OutputOverload = MaxOutput*0.8f;
-    }
+	    OutputOverloadLimit = MaxOutput*0.8f;
+	}
 
     public void SetOutput(object machine, float output)
     {
@@ -48,20 +52,49 @@ public class ScoreUpdater : MonoBehaviour
 
     public void Update()
     {
-        ScoreManager.CityValue += (69 + Random.Range(-3, 3))*Time.deltaTime;
-        Output = _machineOutputs.Values.Sum();
-
-        var outputDiff = Output - MinimumOutputRequired;
-        var incomeChangeMultiplier = EnergySellPrice;
-        if (outputDiff < 0)
-        {
-            incomeChangeMultiplier = EnergyBuyCost;
-        }
-        Income += outputDiff*incomeChangeMultiplier*Time.deltaTime;
-
-	    if (Output > OutputOverload)
-	    {
-		    Application.LoadLevel("Part2");
-	    }
+		UpdateCityValue();
+		UpdateOutput();
+	    UpdateIncome();
+	    CheckOverloading();
     }
+
+	private static void UpdateCityValue()
+	{
+		ScoreManager.CityValue += (69 + Random.Range(-3, 3))*Time.deltaTime;
+	}
+
+	private void UpdateOutput()
+	{
+		Output = Mathf.Min(_machineOutputs.Values.Sum(), MaxOutput);
+	}
+
+	private void UpdateIncome()
+	{
+		var outputDiff = Output - MinimumOutputRequired;
+		var incomeChangeMultiplier = EnergySellPrice;
+		if (outputDiff < 0)
+		{
+			incomeChangeMultiplier = EnergyBuyCost;
+		}
+
+		Income += outputDiff*incomeChangeMultiplier*Time.deltaTime;
+	}
+
+	private void CheckOverloading()
+	{
+		if (Output > OutputOverloadLimit)
+		{
+			var amountOverLimitInUnit = (Output - OutputOverloadLimit)/(MaxOutput - OutputOverloadLimit);
+			OverloadAmount += OverloadPerSecond*amountOverLimitInUnit*Time.deltaTime;
+
+			if (OverloadAmount > 1)
+			{
+				Application.LoadLevel("Part2");
+			}
+		}
+		else
+		{
+			OverloadAmount = 0;
+		}
+	}
 }
