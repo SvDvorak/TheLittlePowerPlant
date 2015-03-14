388511d34a87760a99d1d870d9150a8fa3aa2026
Andreas Wilcox <unknownbum@gmail.com>
2015-03-14 11:17:40 +0100

    Simplified rotation logic

--- Assets/CityGeneration/TileSelector.cs
+++ Assets/CityGeneration/TileSelector.cs
@@ -13,6 +13,8 @@ public class TileSelector : ITileSelector
 	private readonly NonStupidLookup<string, TileTemplate> _connectionToTilesMapping;
 	private readonly TwoDimensionalCollection<PlacedTile> _placedTiles;
 
+	private const int FlipRotation = 2;
+
 	public TileSelector(IConnectionsFinder connectionsFinder)
 	{
 		_connectionsFinder = connectionsFinder;
@@ -36,22 +38,22 @@ public class TileSelector : ITileSelector
 	public object Select(int x, int z)
 	{
 		var requiredConnections = "";
-		int? prevToConnection = null;
+		int rotationToRequiredConnections = 0;
+
 		var leftTile = _placedTiles[x - 1, z];
 		if (leftTile != null)
 		{
-			prevToConnection = 1;
-			requiredConnections += GetOppositeSide(GetSideConnections(leftTile.AllConnections, (5 - leftTile.Rotation) % 4));
+			rotationToRequiredConnections = 1 + FlipRotation;
+			requiredConnections += GetOppositeSide(GetSideConnections(leftTile.AllConnections, 1 - leftTile.Rotation));
 		}
 		var topTile = _placedTiles[x, z - 1];
 		if (topTile != null)
 		{
-			prevToConnection = prevToConnection == null ? 2 : prevToConnection;
-			requiredConnections += GetOppositeSide(GetSideConnections(topTile.AllConnections, (6 - topTile.Rotation) % 4));
+			rotationToRequiredConnections = requiredConnections == null ? 2 + FlipRotation : rotationToRequiredConnections;
+			requiredConnections += GetOppositeSide(GetSideConnections(topTile.AllConnections, 2 - topTile.Rotation));
 		}
 
 		TileTemplate tileTemplate;
-
 		if (requiredConnections != "")
 		{
 			tileTemplate = _connectionToTilesMapping[requiredConnections].First();
@@ -61,13 +63,21 @@ public class TileSelector : ITileSelector
 			tileTemplate = _connectionToTilesMapping.GetTile();
 		}
 
-		PlaceTile(x, z, tileTemplate, prevToConnection != null ? prevToConnection.Value + 2 : 0);
+		PlaceTile(x, z, tileTemplate.Tile, tileTemplate.Rotation + rotationToRequiredConnections);
 		return tileTemplate.Tile;
 	}
 
 	private string GetSideConnections(string allConnections, int rotation)
 	{
-		return (allConnections + allConnections + allConnections).Substring(rotation*2, 2);
+		rotation = GetNormalizedRotation(rotation);
+		return allConnections.Substring(rotation*2, 2);
+	}
+
+	private static int GetNormalizedRotation(int rotation)
+	{
+		rotation = rotation%4;
+		rotation = rotation < 0 ? 4 + rotation : rotation;
+		return rotation;
 	}
 
 	private string GetOppositeSide(string sideConnections)
@@ -76,10 +86,10 @@ public class TileSelector : ITileSelector
 	}
 
 
-	public void PlaceTile(int x, int z, TileTemplate template, int extraRotation)
+	public void PlaceTile(int x, int z, object tile, int rotation)
 	{
-		var completeConnections = _connectionsFinder.GetCompleteConnectionsOriented(template.Tile, template.Rotation);
-		var placedTile = new PlacedTile(template.Tile, completeConnections, (template.Rotation + extraRotation) % 4);
+		var completeConnections = _connectionsFinder.GetCompleteConnectionsOriented(tile, rotation);
+		var placedTile = new PlacedTile(tile, completeConnections, GetNormalizedRotation(rotation));
 		_placedTiles[x, z] = placedTile;
 	}
 }
