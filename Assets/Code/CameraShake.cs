b270b50300444c9b9deb2338b45cfbd760d1687a
Andreas Wilcox <unknownbum@gmail.com>
2015-03-30 23:44:07 +0200

    Improved camera shake to only overwrite shake when it is larger

--- Assets/Code/CameraShake.cs
+++ Assets/Code/CameraShake.cs
@@ -5,6 +5,7 @@ public class CameraShake : MonoBehaviour
 {
 	private LTDescr _easeOutTween;
 	private LTDescr _shakeTween;
+	private float _currentShaking;
 	public float ShakeAmt = 0.2f; // the degrees to shake the camera
 	public float ShakePeriodTime = 0.12f; // The period of each shake
 	public float DropOffTime = 1.6f; // How long it takes the shaking to settle down to nothing
@@ -19,13 +20,23 @@ public class CameraShake : MonoBehaviour
 
 	public void AddCollisionForce(float force)
 	{
+		var newShaking = ShakeAmt*force;
+		if (newShaking < _currentShaking)
+		{
+			return;
+		}
+
 		if (_easeOutTween != null)
 		{
 			_easeOutTween.cancel();
 		}
 
 		_easeOutTween = LeanTween.value(gameObject, ShakeAmt*force, 0f, DropOffTime)
-			.setOnUpdate(val => { _shakeTween.setTo(Vector3.right * val); })
+			.setOnUpdate(val =>
+				{
+					_currentShaking = val;
+					_shakeTween.setTo(Vector3.right * val);
+				})
 			.setEase(LeanTweenType.easeOutQuad);
 	}
 }
