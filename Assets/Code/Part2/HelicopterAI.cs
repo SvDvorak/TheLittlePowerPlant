2c32ef21c2e3f0811af81bc5e8abbc032eef876b
Andreas Wilcox <unknownbum@gmail.com>
2015-04-01 22:37:20 +0200

    Improved helicopter positioning

--- Assets/Code/Part2/HelicopterAI.cs
+++ Assets/Code/Part2/HelicopterAI.cs
@@ -4,20 +4,31 @@ public class HelicopterAI : MonoBehaviour
 {
 	public GameObject AttackTarget;
 	public float FlySpeed;
-	public float TargetKeepDistance;
+	public float MaxSpeedDistance;
+	public float TargetMinDistance;
+	public float TargetMaxDistance;
+	public float NewOffsetTimeInSeconds;
+	public float OffsetConeFromTarget;
+	private Vector3 _offset;
 
 	void Start ()
 	{
+		InvokeRepeating("SetNewRandomOffset", 0, NewOffsetTimeInSeconds);
 	}
 
 	void Update ()
 	{
-		var helicopterToTarget = AttackTarget.transform.position - transform.position;
-		helicopterToTarget.y = 0;
-		var normalizedToTarget = helicopterToTarget.normalized;
-		var helicopterToTargetOffset = helicopterToTarget - normalizedToTarget*TargetKeepDistance;
+		var helicopterToTargetOffset = (_offset + AttackTarget.transform.position) - transform.position;
+		helicopterToTargetOffset.y = 0;
 
-		transform.position += helicopterToTargetOffset.normalized*FlySpeed*Time.deltaTime;
-		transform.LookAt(helicopterToTarget + transform.position);
+		var velocity = Mathf.SmoothStep(0, FlySpeed, helicopterToTargetOffset.magnitude/MaxSpeedDistance);
+		transform.position += helicopterToTargetOffset.normalized*velocity*Time.deltaTime;
+		transform.LookAt(AttackTarget.transform.position);
+	}
+
+	private void SetNewRandomOffset()
+	{
+		var randomDirection = Quaternion.Euler(0, UnityEngine.Random.Range(-OffsetConeFromTarget, OffsetConeFromTarget), 0)*(-Vector3.forward);
+		_offset = randomDirection*UnityEngine.Random.Range(TargetMinDistance, TargetMaxDistance);
 	}
 }
