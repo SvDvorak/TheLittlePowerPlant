2a982058ebcf3bc80cbbe416326d21efda615345
Andreas Wilcox <UnknownBum@gmail.com>
2015-04-01 16:19:45 +0200

    Started on helicopter AI

---
+++ Assets/Code/Part2/HelicopterAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class HelicopterAI : MonoBehaviour
+{
+	public GameObject AttackTarget;
+	public float FlySpeed;
+	public float TargetKeepDistance;
+
+	void Start ()
+	{
+	}
+
+	void Update ()
+	{
+		var helicopterToTarget = AttackTarget.transform.position - transform.position;
+		helicopterToTarget.y = 0;
+		var normalizedToTarget = helicopterToTarget.normalized;
+		var helicopterToTargetOffset = helicopterToTarget - normalizedToTarget*TargetKeepDistance;
+
+		transform.position += helicopterToTargetOffset.normalized*FlySpeed*Time.deltaTime;
+		transform.LookAt(helicopterToTarget + transform.position);
+	}
+}
