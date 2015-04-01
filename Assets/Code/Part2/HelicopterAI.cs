964634d79bdb9f545aec88030507fb9071feae64
Andreas Wilcox <unknownbum@gmail.com>
2015-04-02 00:11:20 +0200

    Basic missile firing from helicopters

--- Assets/Code/Part2/HelicopterAI.cs
+++ Assets/Code/Part2/HelicopterAI.cs
@@ -9,11 +9,18 @@ public class HelicopterAI : MonoBehaviour
 	public float TargetMaxDistance;
 	public float NewOffsetTimeInSeconds;
 	public float OffsetConeFromTarget;
+	public float FireDelay;
+	public GameObject MissileTemplate;
+	public Transform MissileSpawnL;
+	public Transform MissileSpawnR;
+
 	private Vector3 _offset;
+	private bool _fireFromLeft;
 
 	void Start ()
 	{
 		InvokeRepeating("SetNewRandomOffset", 0, NewOffsetTimeInSeconds);
+		InvokeRepeating("FireMissile", FireDelay, FireDelay);
 	}
 
 	void Update ()
@@ -30,5 +37,25 @@ public class HelicopterAI : MonoBehaviour
 	{
 		var randomDirection = Quaternion.Euler(0, UnityEngine.Random.Range(-OffsetConeFromTarget, OffsetConeFromTarget), 0)*(-Vector3.forward);
 		_offset = randomDirection*UnityEngine.Random.Range(TargetMinDistance, TargetMaxDistance);
+	}
+
+	private void FireMissile()
+	{
+		if (_fireFromLeft)
+		{
+			SpawnMissile(MissileSpawnL);
+		}
+		else
+		{
+			SpawnMissile(MissileSpawnR);
+		}
+
+		_fireFromLeft = !_fireFromLeft;
+	}
+
+	private void SpawnMissile(Transform missileSpawn)
+	{
+		var missile = Instantiate(MissileTemplate);
+		missile.transform.position = transform.position + missileSpawn.position;
 	}
 }
---
+++ Assets/Code/Part2/MissileMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileMovement : MonoBehaviour
+{
+	void Start ()
+	{
+	
+	}
+	
+	void Update ()
+	{
+		transform.position += new Vector3(0, 0, 1*Time.deltaTime);
+	}
+}
