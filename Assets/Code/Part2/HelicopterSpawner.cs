71e386c274277e35a494c5eeabd3082d34279dd8
Andreas Wilcox <unknownbum@gmail.com>
2015-04-01 22:51:26 +0200

    Added helicopter spawner

---
+++ Assets/Code/Part2/HelicopterSpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelicopterSpawner : MonoBehaviour
+{
+	public GameObject HelicopterTemplate;
+	public GameObject AttackTarget;
+	public float MaxOffsetInX;
+	public float HeightOffset;
+	public float SpawnTime;
+	private float _timeSinceLastSpawn;
+	private float _timeTillNextSpawn;
+
+	void Start ()
+	{
+		_timeTillNextSpawn = GetNextSpawnTime();
+	}
+	
+	void Update ()
+	{
+		if (_timeSinceLastSpawn > _timeTillNextSpawn)
+		{
+			_timeSinceLastSpawn = 0;
+			_timeTillNextSpawn = GetNextSpawnTime();
+			SpawnHelicopter();
+		}
+
+		_timeSinceLastSpawn += Time.deltaTime;
+	}
+
+	private float GetNextSpawnTime()
+	{
+		return SpawnTime + UnityEngine.Random.Range(-2, 2);
+	}
+
+	private void SpawnHelicopter()
+	{
+		var helicopter = Instantiate(HelicopterTemplate);
+		helicopter.transform.position = AttackTarget.transform.position + new Vector3(UnityEngine.Random.Range(-MaxOffsetInX, MaxOffsetInX), HeightOffset, -120);
+		helicopter.GetComponent<HelicopterAI>().AttackTarget = AttackTarget;
+	}
+}
