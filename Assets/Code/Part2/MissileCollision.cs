49fd2e140c41791de9bc82e56c1c2336b72b5de5
Andreas Wilcox <unknownbum@gmail.com>
2015-04-04 11:12:33 +0200

    Missile collision

---
+++ Assets/Code/Part2/MissileCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Code;
+
+public class MissileCollision : MonoBehaviour
+{
+	public float MissileDamage;
+
+	void OnTriggerEnter(Collider other)
+	{
+		var collidedName = other.gameObject.name.ToUpper();
+		if (collidedName.Contains("TRANSFORMER"))
+		{
+			var dataContext = other.gameObject.GetDataContext<GameState>();
+
+			dataContext.Health -= MissileDamage;
+		}
+
+		Destroy(gameObject);
+	}
+}
