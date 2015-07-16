86333b32955ce24c62a247e99e9a89913c7eaf49
Andreas Wilcox <unknownbum@gmail.com>
2015-07-16 11:52:50 +0200

    Made trees fall right direction

--- Assets/Code/Part2/TreeFall.cs
+++ Assets/Code/Part2/TreeFall.cs
@@ -4,17 +4,18 @@ public class TreeFall : MonoBehaviour
 {
 	private const float MaxTime = 1;
 	private float _animationTime;
-	private Vector3 _startingOrientation;
 
 	public void Start ()
 	{
 		_animationTime = 0;
-		_startingOrientation = transform.eulerAngles;
 	}
 
 	public void Update()
 	{
-		transform.eulerAngles = _startingOrientation + new Vector3(Mathf.Clamp(_animationTime/MaxTime, 0, 1)*90, 0, 0);
+        if(_animationTime < MaxTime)
+        {
+            transform.RotateAround(transform.position, Vector3.left, (Time.deltaTime*90)/MaxTime);
+        }
 		_animationTime += Time.deltaTime;
 	}
 }
