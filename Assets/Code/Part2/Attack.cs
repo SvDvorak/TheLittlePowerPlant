c90230cc60a216c12adc1a1c7c3b8121657d92d6
Andreas Wilcox <unknownbum@gmail.com>
2015-07-16 23:49:49 +0200

    Moved around files and started weapon implementation

---
+++ Assets/Code/Part2/Attack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Code;
+
+public class Attack : MonoBehaviour
+{
+    public LaserLogic Weapon;
+    private GameState _gameState;
+
+    void Start ()
+	{
+		_gameState = gameObject.GetDataContext<GameState>();
+    }
+
+    void Update ()
+	{
+        if (_gameState.IsAlive && Input.GetMouseButton(1))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Weapon.FireAt(hit.point);
+            }
+        }
+    }
+}
---
+++ Assets/Code/Part2/LaserLogic.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserLogic : MonoBehaviour
+{
+    private Vector3 _target;
+
+    void Start ()
+	{
+	
+	}
+
+	void Update ()
+	{
+	    if (_target != null)
+	    {
+            var ray = new Ray(transform.position, _target - transform.position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                var startToEnd = hit.point - transform.position;
+
+                var localScale = transform.localScale;
+                localScale.z = startToEnd.magnitude;
+                transform.localScale = localScale;
+                transform.rotation = Quaternion.LookRotation(-startToEnd, Vector3.up);
+            }
+        }
+    }
+
+    public void FireAt(Vector3 target)
+    {
+        _target = target;
+    }
+}
